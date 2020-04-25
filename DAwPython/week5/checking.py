009dadd220a63b185e5c00768d49a29903164722
ApuCSE <37530659+ApuCSE@users.noreply.github.com>
2020-04-26 01:48:33 +0600

    Add files via upload

---
+++ DAwPython/week5/checking.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 23:59:28 2020
+
+@author: apu
+"""
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import Ridge
+from sklearn.model_selection import GridSearchCV
+
+#path='https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/module_5_auto.csv'
+#df=pd.read_csv(path)
+#df.head()
